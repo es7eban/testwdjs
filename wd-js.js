1eabff786fef950cc5fb95308e7a288671f541e2
Esteban Arredondo <es_7eban@hotmail.com>
2022-01-31 10:23:28 +0100

    Update wd-js.js

--- wd-js.js
+++ wd-js.js
@@ -785,9 +785,11 @@ var NodeFilter = function () {
   };
 
   NodeFilter.prototype.processChildElement = function (nodeElement) {
-    console.log('####asdfadfasdfasdf####'); // if (nodeElement.parentElement?.getAttribute(`data-dgd${this.keyPhrase.name.replace(/\s+/g, '')}`) === 'visited') {
+    var _a;
 
-    if (nodeElement.getAttribute("data-dgd" + this.keyPhrase.name.replace(/\s+/g, '')) === 'visited') {
+    console.log('####processChildElement asdfadfasdfasdf 10:19####');
+
+    if (nodeElement.attributes && nodeElement.getAttribute("data-dgd" + this.keyPhrase.name.replace(/\s+/g, '')) === 'visited' || ((_a = nodeElement.parentElement) === null || _a === void 0 ? void 0 : _a.getAttribute("data-dgd" + this.keyPhrase.name.replace(/\s+/g, ''))) === 'visited') {
       return;
     }
 
@@ -796,6 +798,8 @@ var NodeFilter = function () {
 
   NodeFilter.prototype.ifMatchKeyphraseStoreElement = function (nodeElement) {
     var _a, _b;
+
+    console.log('####ifMatchKeyphraseStoreElement asdfadfasdfasdf 10:19####');
 
     if ((_a = nodeElement.parentElement) === null || _a === void 0 ? void 0 : _a.innerHTML) {
       if (nodeElement.attributes) {
