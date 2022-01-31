3792349f3da0370b426bc02a0bbae2596f733a35
Esteban Arredondo <es_7eban@hotmail.com>
2022-01-31 21:38:56 +0100

    Update wd-js.js

--- wd-js.js
+++ wd-js.js
@@ -933,4 +933,8 @@ exports.NodeSelector = NodeSelector;
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
-/
+/******/ 	// This entry module is referenced by other modules so it can't be inlined
+/******/ 	var __webpack_exports__ = __webpack_require__("./resources/js/index.ts");
+/******/ 	
+/******/ })()
+;
