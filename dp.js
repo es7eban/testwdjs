892a13446919a250ae45a2e11fe61072bf8976f8
Esteban Arredondo <es_7eban@hotmail.com>
2022-02-01 17:08:47 +0100

    Update dp.js

--- dp.js
+++ dp.js
@@ -23,7 +23,7 @@ var Digidip = 1;
         element_ignore_consider_parents: "0",   // if or if not parent elements shall be considered (string; "1" or "0")
         widgets: JSON.parse('{}'),
         project: '990',
-        worddip_cdn_url: 'https://cdn.jsdelivr.net/gh/es7eban/testwdjs@6ef2cf85d1680d72b882cd83d0bcc57ffe7bf48d/wd-js.js',
+        worddip_cdn_url: 'https://cdn.jsdelivr.net/gh/es7eban/testwdjs@2beb973876331303ab6fc96e8faec6d77c0d3a3c/wd-js.js',
         worddip_api_hostname: 'wds.digidip.net',
 
         // further settings, will not be changed by JS handler
