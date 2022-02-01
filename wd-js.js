2a27cac1b983500629d292de954b22c144a6bb12
Esteban Arredondo <es_7eban@hotmail.com>
2022-02-01 18:43:47 +0100

    Update wd-js.js

--- wd-js.js
+++ wd-js.js
@@ -776,7 +776,7 @@ var NodeFilter = function () {
       // console.log('node.nodeName 16:06===>', node.nodeName);
       node.childNodes.forEach(this.scanForChildTextNodes.bind(this));
     } else {
-      console.log('processing (node as Element).innerHTML ===>', node.innerHTML);
+      console.log("processing (node as Element).innerHTML " + this.keyPhrase.name + " ===>", node.innerHTML);
       this.processNode(node);
     }
   };
